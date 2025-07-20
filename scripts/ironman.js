d7e18c30c27ddea1347644ae6a4bab7d11557598
landonedwards <131121236+landonedwards@users.noreply.github.com>
2025-07-20 04:27:13 -0400

    merge selected game mode data with global characters array

--- scripts/ironman.js
+++ scripts/ironman.js
@@ -1,55 +1,7 @@
-// const characters = [
-//     { id: 1, charName: "Eliwood", HP: 80, Str: 45, Mag: 0, Dex: 50, Spd: 40, Lck: 45, Def: 30, Res: 35, image: "images/char-sprite/eliwood.png", joinChapter: 11, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 2, charName: "Lyn", HP: 70, Str: 40, Mag: 0, Dex: 60, Spd: 60, Lck: 55, Def: 20, Res: 30, image: "images/char-sprite/lyn.png", joinChapter: 15, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 3, charName: "Hector", HP: 90, Str: 60, Mag: 0, Dex: 45, Spd: 35, Lck: 30, Def: 50, Res: 25, image: "images/char-sprite/hector.png", joinChapter: 12, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 4, charName: "Sain", HP: 80, Str: 60, Mag: 0, Dex: 35, Spd: 40, Lck: 35, Def: 20, Res: 20, image: "images/char-sprite/sain.png", joinChapter: 15, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 5, charName: "Kent", HP: 85, Str: 40, Mag: 0, Dex: 50, Spd: 45, Lck: 20, Def: 25, Res: 25, image: "images/char-sprite/kent.png", joinChapter: 15, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 6, charName: "Florina", HP: 60, Str: 40, Mag: 0, Dex: 50, Spd: 55, Lck: 50, Def: 15, Res: 35, image: "images/char-sprite/florina.png", joinChapter: 15, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 7, charName: "Wil", HP: 75, Str: 50, Mag: 0, Dex: 50, Spd: 40, Lck: 40, Def: 20, Res: 25, image: "images/char-sprite/wil.png", joinChapter: 15, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 8, charName: "Dorcas", HP: 80, Str: 60, Mag: 0, Dex: 40, Spd: 20, Lck: 45, Def: 25, Res: 15, image: "images/char-sprite/dorcas.png", joinChapter: 11, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 9, charName: "Serra", HP: 50, Str: 0, Mag: 50, Dex: 30, Spd: 40, Lck: 60, Def: 15, Res: 55, image: "images/char-sprite/serra.png", joinChapter: 13, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 10, charName: "Erk", HP: 65, Str: 0, Mag: 40, Dex: 40, Spd: 50, Lck: 30, Def: 20, Res: 40, image: "images/char-sprite/erk.png", joinChapter: 14, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 11, charName: "Rath", HP: 80, Str: 50, Mag: 0, Dex: 40, Spd: 50, Lck: 30, Def: 10, Res: 25, image: "images/char-sprite/rath.png", joinChapter: 21, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 12, charName: "Matthew", HP: 75, Str: 30, Mag: 0, Dex: 40, Spd: 70, Lck: 50, Def: 25, Res: 20, image: "images/char-sprite/matthew.png", joinChapter: 13, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 13, charName: "Wallace", HP: 70, Str: 45, Mag: 0, Dex: 40, Spd: 20, Lck: 30, Def: 35, Res: 35, image: "images/char-sprite/wallace.png", joinChapter: 23, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 14, charName: "Lowen", HP: 90, Str: 30, Mag: 0, Dex: 30, Spd: 30, Lck: 50, Def: 40, Res: 30, image: "images/char-sprite/lowen.png", joinChapter: 11, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 15, charName: "Rebecca", HP: 60, Str: 40, Mag: 0, Dex: 50, Spd: 60, Lck: 50, Def: 15, Res: 30, image: "images/char-sprite/rebecca.png", joinChapter: 11, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 16, charName: "Marcus", HP: 65, Str: 30, Mag: 0, Dex: 50, Spd: 25, Lck: 30, Def: 15, Res: 35, image: "images/char-sprite/marcus.png", joinChapter: 11, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 17, charName: "Bartre", HP: 85, Str: 50, Mag: 0, Dex: 35, Spd: 40, Lck: 30, Def: 30, Res: 25, image: "images/char-sprite/bartre.png", joinChapter: 11, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 18, charName: "Oswin", HP: 90, Str: 40, Mag: 0, Dex: 30, Spd: 30, Lck: 35, Def: 55, Res: 30, image: "images/char-sprite/oswin.png", joinChapter: 12, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 19, charName: "Guy", HP: 75, Str: 30, Mag: 0, Dex: 50, Spd: 70, Lck: 45, Def: 15, Res: 25, image: "images/char-sprite/guy.png", joinChapter: 13, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 20, charName: "Merlinus", HP: 120, Str: 0, Mag: 0, Dex: 90, Spd: 90, Lck: 100, Def: 30, Res: 15, image: "images/char-sprite/merlinus.png", joinChapter: 14, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 21, charName: "Priscilla", HP: 45, Str: 0, Mag: 40, Dex: 50, Spd: 40, Lck: 65, Def: 15, Res: 50, image: "images/char-sprite/priscilla.png", joinChapter: 14, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 22, charName: "Raven", HP: 85, Str: 55, Mag: 0, Dex: 40, Spd: 45, Lck: 35, Def: 25, Res: 15, image: "images/char-sprite/raven.png", joinChapter: 16, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 23, charName: "Lucius", HP: 55, Str: 0, Mag: 60, Dex: 50, Spd: 40, Lck: 20, Def: 10, Res: 60, image: "images/char-sprite/lucius.png", joinChapter: 16, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 24, charName: "Canas", HP: 70, Str: 45, Mag: 0, Dex: 40, Spd: 35, Lck: 25, Def: 25, Res: 45, image: "images/char-sprite/canas.png", joinChapter: 16, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 25, charName: "Dart", HP: 70, Str: 65, Mag: 0, Dex: 20, Spd: 60, Lck: 35, Def: 20, Res: 15, image: "images/char-sprite/dart.png", joinChapter: 18, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 26, charName: "Fiora", HP: 70, Str: 35, Mag: 0, Dex: 60, Spd: 50, Lck: 30, Def: 20, Res: 50, image: "images/char-sprite/fiora.png", joinChapter: 18, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 27, charName: "Legault", HP: 60, Str: 25, Mag: 0, Dex: 45, Spd: 60, Lck: 60, Def: 25, Res: 25, image: "images/char-sprite/legault.png", joinChapter: 19, status: "alive", deathChapter: null, deathNote: null },
-//     // Add nils for the chapters he's in and then swap back to ninian
-//     { id: 28, charName: "Ninian", HP: 85, Str: 5, Mag: 0, Dex: 5, Spd: 70, Lck: 80, Def: 30, Res: 70, image: "images/char-sprite/ninian.png", joinChapter: 20, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 29, charName: "Isadora", HP: 75, Str: 30, Mag: 0, Dex: 35, Spd: 50, Lck: 45, Def: 20, Res: 25, image: "images/char-sprite/isadora.png", joinChapter: 21, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 30, charName: "Heath", HP: 80, Str: 50, Mag: 0, Dex: 50, Spd: 45, Lck: 20, Def: 30, Res: 20, image: "images/char-sprite/heath.png", joinChapter: 21, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 31, charName: "Hawkeye", HP: 50, Str: 40, Mag: 0, Dex: 30, Spd: 25, Lck: 40, Def: 20, Res: 35, image: "images/char-sprite/hawkeye.png", joinChapter: 22, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 32, charName: "Geitz", HP: 85, Str: 50, Mag: 0, Dex: 30, Spd: 40, Lck: 40, Def: 20, Res: 20, image: "images/char-sprite/geitz.png", joinChapter: 23, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 33, charName: "Farina", HP: 75, Str: 50, Mag: 0, Dex: 40, Spd: 45, Lck: 45, Def: 25, Res: 30, image: "images/char-sprite/farina.png", joinChapter: 999, status: "alive", deathChapter: null, deathNote: null }, // PLACEHOLDER because Hector Mode only (will implement later)
-//     { id: 34, charName: "Pent", HP: 50, Str: 0, Mag: 30, Dex: 20, Spd: 40, Lck: 40, Def: 30, Res: 35, image: "images/char-sprite/pent.png", joinChapter: 24, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 35, charName: "Louise", HP: 60, Str: 40, Mag: 0, Dex: 40, Spd: 40, Lck: 30, Def: 20, Res: 30, image: "images/char-sprite/louise.png", joinChapter: 24, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 36, charName: "Karel", HP: 70, Str: 30, Mag: 0, Dex: 50, Spd: 50, Lck: 30, Def: 10, Res: 15, image: "images/char-sprite/karel.png", joinChapter: 25, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 37, charName: "Harken", HP: 80, Str: 35, Mag: 0, Dex: 30, Spd: 40, Lck: 20, Def: 30, Res: 25, image: "images/char-sprite/harken.png", joinChapter: 25, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 38, charName: "Nino", HP: 55, Str: 0, Mag: 50, Dex: 55, Spd: 60, Lck: 45, Def: 15, Res: 50, image: "images/char-sprite/nino.png", joinChapter: 26, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 39, charName: "Jaffar", HP: 65, Str: 15, Mag: 0, Dex: 40, Spd: 35, Lck: 20, Def: 30, Res: 30, image: "images/char-sprite/jaffar.png", joinChapter: 26, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 40, charName: "Vaida", HP: 60, Str: 45, Mag: 0, Dex: 25, Spd: 40, Lck: 30, Def: 25, Res: 15, image: "images/char-sprite/vaida.png", joinChapter: 27, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 41, charName: "Karla", HP: 60, Str: 25, Mag: 0, Dex: 45, Spd: 55, Lck: 40, Def: 10, Res: 20, image: "images/char-sprite/karla.png", joinChapter: 999, status: "alive", deathChapter: null, deathNote: null }, // PLACEHOLDER because Hector Mode only (will implement later)
-//     { id: 42, charName: "Renault", HP: 60, Str: 40, Mag: 0, Dex: 30, Spd: 35, Lck: 15, Def: 20, Res: 40, image: "images/char-sprite/renault.png", joinChapter: 30, status: "alive", deathChapter: null, deathNote: null },
-//     { id: 43, charName: "Athos", HP: 0, Str: 0, Mag: 0, Dex: 0, Spd: 0, Lck: 0, Def: 0, Res: 0, image: "images/char-sprite/athos.png", joinChapter: 31, status: "alive", deathChapter: null, deathNote: null }
-// ];
-
 // variable declarations 
 
 let characters;
 let gameModes;
-let chapters;
 let currentSelectedGameMode;
 
 // key for storing dead characters in localStorage
@@ -96,7 +48,7 @@ function loadGameDataFromStorage() {
 
     // parse the string into an object
     const loadedGameData = JSON.parse(savedGameDataString);
-
+    // grab selected game mode
     const currentSelectedModeId = localStorage.getItem("selectedGameMode");
     // check if loaded save data matches current selected mode
     if (loadedGameData.selectedGameModeId != currentSelectedModeId) {
@@ -141,16 +93,42 @@ function saveGameDataToStorage() {
     localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(saveData));
 }
 
+// for use in populateChapterDropdown() to convert nonnumeric chapter values
+function getNumericChapterValue(stringChapter) {
+    let xCount = 0;
+    // grab integer from chapter value 
+    let initialValue = parseInt(stringChapter);
+
+    // for each letter in chapter value
+    for (let i = 1; i < stringChapter.length; i++) {
+        if (stringChapter[i] == "x") {
+            xCount ++;
+        } 
+    }
+
+    // if chapter is a gaiden chapter (ex: 5x)...
+    if (xCount == 1) {
+        // add to its numeric value
+        initialValue += 0.5
+    }
+    else if (xCount > 1) {
+        // if chapter is a gaiden chapter following a gaiden, add an increased value to it (for comparison purposes)
+        initialValue += 0.75
+    }
+
+    return parseFloat(initialValue);
+}
+
 function populateChapterDropdown(gameMode) {
-    // clear dropdown options so they don't stack
+    // clear dropdown options so they don't stack and initialize base value 
     chapSelect.innerHTML = `<option value="">All</option>`;
     // populate chapter dropdown based on what mode you're playing
-    for (let i = gameMode.startChapter; i <= gameMode.endChapter; i++) {
+    gameMode.chapters.forEach(chapter => {
         const chapterNum = document.createElement("option");
-        chapterNum.value = i;
-        chapterNum.textContent = `Chapter ${i}`;
+        chapterNum.value = chapter.number;
+        chapterNum.textContent = `Chapter ${chapter.number}: ${chapter.name}`;
         chapSelect.append(chapterNum);
-    }
+    })
 
     chapSelect.selectedIndex = 1;
     currentChapter = chapSelect.value;
@@ -158,13 +136,15 @@ function populateChapterDropdown(gameMode) {
 
 // filter helper function
 function filterJoinChapter(character) {
-    return character.joinChapter <= chapSelect.value;
+    // convert all chapter values (gaiden chapters are initially strings) to numbers for comparison between join chapter and selected chapter
+    const selectedChapter = getNumericChapterValue(chapSelect.value);
+    return character.joinChapter <= selectedChapter;
 }
 
 function filterCharactersByChapter() {
-    const chapSelected = chapSelect.value;
+    const selectedChapter = chapSelect.value;
     // if no specific chapter is selected, display characters for all chapters.
-    if (chapSelected == "All" || chapSelected == "") {
+    if (selectedChapter == "All" || selectedChapter == "") {
         return characters;
     }
     // get and return filtered characters
@@ -324,6 +304,7 @@ function promptForDeathNote(character, characterDisplayElement) {
         // if user leaves it blank, provide a generic death description
         if (deathNote == "") {
             deathNote = "Died fighting for a better future.";
+            saveDeathNote(character, deathNote, characterDisplayElement);
         }
         saveDeathNote(character, deathNote, characterDisplayElement);
     });
@@ -389,7 +370,6 @@ async function init() {
         // assign values to global variables based on fetched game data
         characters = fetchedData.characters;
         gameModes = fetchedData.gameModes;
-        chapters = fetchedData.chapters;
         const selectedModeIdFromStorage = localStorage.getItem("selectedGameMode");
 
         // if user has selected a mode, set global variable
@@ -400,7 +380,7 @@ async function init() {
         // if user has not selected a mode...
         if (!currentSelectedGameMode) {
             // set default to Eliwood mode
-            currentSelectedGameMode = gameModes.find(mode => mode.name == "Eliwood's Story");
+            currentSelectedGameMode = gameModes.find(mode => mode.id == 2);
 
             // if default doesn't exist, return and stop function
             if (!currentSelectedGameMode) {
@@ -408,6 +388,22 @@ async function init() {
                 return;
             }
         }
+
+        const modeCharacters = currentSelectedGameMode.charactersInMode.map(modeChar => {
+            const baseChar = characters.find(char => char.id == modeChar.charId);
+            if (baseChar) {
+                return {
+                    // copy all properties from original character
+                    ...baseChar,
+                    // set the character's join chapter for this mode and merge it into one character array (specific to this mode)
+                    joinChapter: modeChar.joinChapter
+                };
+            }
+            return null;
+        }) // could add .filter(Boolean) to remove any nonvalid characters created 
+
+        // assign new array with correct join chapter values to global characters array
+        characters = modeCharacters;
 
         // call key functions needed for startup
         populateChapterDropdown(currentSelectedGameMode);
@@ -433,8 +429,6 @@ async function init() {
         console.error("Failed to load game data. Tracker may not function correctly.");
     }
 }
-
-// FUNCTION TO DISPLAY GAMEOVER MODAL (WHEN LORD DIES)
 
 // event listeners
 
